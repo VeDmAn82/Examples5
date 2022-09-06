cb6585dd8e61386f855490019b17e7c05263eebb
Dmitry Vechkhayzer <dmitry.v82@mail.ru>
2022-09-06 19:46:10 +0400

    Seminar6

---
+++ Seminar6/Program.cs
@@ -0,0 +1,59 @@
+﻿// Проверка треугольника
+
+// Console.WriteLine("Первое число:  ");
+// bool isNumber1 = int.TryParse(Console.ReadLine(), out int x);
+// Console.WriteLine("Второе число:  ");
+// bool isNumber2 = int.TryParse(Console.ReadLine(), out int y);
+// Console.WriteLine("Третье число:  ");
+// bool isNumber3 = int.TryParse(Console.ReadLine(), out int z);
+
+// if (!isNumber1 || !isNumber2 || !isNumber2)
+// {
+//     Console.WriteLine("Invalid number");
+//     return;
+// }
+
+// bool result = CheckTriangle(x, y, z);
+// Console.WriteLine(result);
+// bool CheckTriangle(int a, int b, int c)
+// {
+//     if(x >= y + z)
+//     {
+//         return false;
+//     }
+//     if(y >= x + z)
+//     {
+//         return false;
+//     }
+//     if(z >= x + y)
+//     {
+//         return false;
+//     }
+//     return true;
+// }
+
+
+// Получить двоичное число из десятичного
+
+// Console.WriteLine("Введите десятичное число:");
+// int i = Convert.ToInt32(Console.ReadLine());
+// string s = Convert.ToString(i, 2);
+// Console.WriteLine("В двоичной системе счисления это: " + s);
+
+
+
+
+// string str = ConvertNumber(x);
+// string ConvertNumber(int x)
+// {
+//     string result = "";
+//     while(x > 0)
+//     {
+//         int y = x % 2;
+//         result = Convert.ToString(y) + result;
+//         x = x / 2;
+//     }
+//     return result;
+// }
+// Console.WriteLine(str);
+
